dd97a06e39675e250fed0ef4d5877663935bc958
Bilal <belkhoul@uoguelph.ca>
2023-11-22 15:35:13 +0400

    changed enviro variable for postgresql

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def load_user(user_id):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
